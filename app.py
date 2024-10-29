c0b4f5a11729dada78cd8ec9561714257fa17288
LucasKarlsten <lucas.karlsten@gmail.com>
2024-10-29 12:02:02 +0100

    Update app.py
    Liten struktur för fastapi, säkert mycket ändrigar som ska göras

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
-from fastapi import Fastapi
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class AllaRum:
+    # __init__ ?
+    rum_id: int
+    alla_rum = [None]
+    bokade_rum = [None]
+    obokade_rum = [None]
+    bokat: bool = False
+
+    class BokadeRum:
+        bokat: bool = True
+        bokade_rum = [None]
+
+    class ObokadeRum:
+        bokat: bool = False
+
+
+@app.get("/")
+async def root():
+    return {'Message ' : 'Välkommen till Grupp 9:s bokingssystem'}
+
+@app.get("/rum")
+async def kolla_bokingar():
+    return AllaRum.alla_rum
+
+
+@app.get("/rum/bokat")
+async def bokade_rum():
+    return AllaRum.bokade_rum
+
+@app.get("rum/obokade")
+async def obokade_rum():
+    return AllaRum.obokade_rum
+
+@app.post("/boka")
+async def ny_bokning():
+    nuvarande_rum = AllaRum.rum_id
+    if nuvarande_rum(AllaRum.bokat) == False:
+        print('Rum bokat')
